2790a6eff0d27db77ded8e9c97df6deca5e58858
tmayweather <tnea.mayweather@gmail.com>
2023-10-23 14:38:41 -0400

    tf-controller

--- terraform/kubernetes/cloudflare/main.tf
+++ terraform/kubernetes/cloudflare/main.tf
@@ -1,36 +1,32 @@
 terraform {
-    cloud {
-      hostname = "app.terraform.io"
-      organization = "lowkey"
-
-      workspaces {
-        name = "home-cloudflare"
-      }
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "lowkey"
+    workspaces {
+      name = "home-cloudflare"
     }
-    required_providers {
-      cloudflare = {
-        source = "cloudflare/cloudflare"
-        version = "4.17.0"
-      }
-      http = {
-        source = "hashicorp/http"
-        version = "3.4.0"
-      }
-      onepassword = {
-        version = "1.2.1"
-        source  = "1password/onepassword"
+  }
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.17.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.0"
+    }
+  }
+  required_version = ">= 1.3.0"
 }
-
-    }
-    required_version = ">= 1.3.0"
-
-    }
-
 
 module "onepassword_item" {
   source = "github.com/bjw-s/terraform-1password-item?ref=main"
   vault  = "Kubernetes"
   item   = "cloudflare"
+}
+
+data "http" "ipv4_lookup_raw" {
+  url = "http://ipv4.icanhazip.com"
 }
 
 data "cloudflare_zone" "domain" {
